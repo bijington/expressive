7307049afcfeeb7f5cc7f36f06e9d861e08aca8a
Shaun Lawrence <shaunrlawrence@gmail.com>
2016-05-26 14:56:32 +0100

    Added in the missing RegexFunction file

---
+++ Source/CSharp/Expressive/Expressive/Functions/RegexFunction.cs
@@ -0,0 +1,21 @@
+﻿using Expressive.Expressions;
+using System.Text.RegularExpressions;
+
+namespace Expressive.Functions
+{
+    internal class RegexFunction : FunctionBase
+    {
+        #region FunctionBase Members
+
+        public override string Name { get { return "Regex"; } }
+
+        public override object Evaluate(IExpression[] participants)
+        {
+            this.ValidateParameterCount(participants, 2, 2);
+            
+            return new Regex(participants[1].Evaluate(Arguments) as string).IsMatch(participants[0].Evaluate(Arguments) as string);
+        }
+
+        #endregion
+    }
+}
