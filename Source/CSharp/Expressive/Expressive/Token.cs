801559c2c765cce6dceecadceba0c0d5ee124dcc
Shaun Lawrence <shaunrlawrence@gmail.com>
2016-08-11 07:50:56 +0100

    Added in the missing Token file

---
+++ Source/CSharp/Expressive/Expressive/Token.cs
@@ -0,0 +1,18 @@
+﻿namespace Expressive
+{
+    internal sealed class Token
+    {
+        internal string CurrentToken { get; private set; }
+
+        internal int Length { get; private set; }
+
+        internal int StartIndex { get; private set; }
+
+        public Token(string currentToken, int startIndex)
+        {
+            this.CurrentToken = currentToken;
+            this.StartIndex = startIndex;
+            this.Length = this.CurrentToken?.Length ?? 0;
+        }
+    }
+}
