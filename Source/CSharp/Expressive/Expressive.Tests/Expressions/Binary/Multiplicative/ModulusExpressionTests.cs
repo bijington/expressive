23f2a7c4d7a5db31169f8fee145f9f46deb3245b
Shaun Lawrence <shaunrlawrence@gmail.com>
2018-12-29 18:17:53 +0000

    Completed coverage of binary expressions.

---
+++ Source/CSharp/Expressive/Expressive.Tests/Expressions/Binary/Multiplicative/ModulusExpressionTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Expressive.Expressions;
+using Expressive.Expressions.Binary.Multiplicative;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Expressive.Tests.Expressions.Binary.Multiplicative
+{
+    [TestClass]
+    public class ModulusExpressionTests
+    {
+        [TestMethod]
+        public void TestEvaluate()
+        {
+            var expression = new ModulusExpression(
+                Mock.Of<IExpression>(e => e.Evaluate(It.IsAny<IDictionary<string, object>>()) == (object)5),
+                Mock.Of<IExpression>(e => e.Evaluate(It.IsAny<IDictionary<string, object>>()) == (object)2),
+                ExpressiveOptions.None);
+
+            Assert.AreEqual(1, expression.Evaluate(null));
+        }
+    }
+}
