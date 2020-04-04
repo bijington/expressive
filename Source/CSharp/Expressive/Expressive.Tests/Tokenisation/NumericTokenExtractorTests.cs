050120bdea236cf506fd809c28dc2b39314c4357
Shaun Lawrence <bijington@tinysoftltd.onmicrosoft.com>
2020-04-04 22:52:34 +0100

    Added in the ability to specify scientific notation in an expression.

--- Source/CSharp/Expressive/Expressive.Tests/Tokenisation/NumericTokenExtractorTests.cs
+++ Source/CSharp/Expressive/Expressive.Tests/Tokenisation/NumericTokenExtractorTests.cs
@@ -1,4 +1,5 @@
-﻿using Expressive.Tokenisation;
+﻿using System.Globalization;
+using Expressive.Tokenisation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Expressive.Tests.Tokenisation
@@ -18,6 +19,39 @@ namespace Expressive.Tests.Tokenisation
         }
 
         [TestMethod]
+        public void TestWithDecimalPlaceLeading()
+        {
+            var extractor = new NumericTokenExtractor();
+
+            var token = extractor.ExtractToken(".45", 0, new Context(ExpressiveOptions.None));
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual(".45", token.CurrentToken);
+        }
+
+        [TestMethod]
+        public void TestWithDecimalPlaceAndNegativeSign()
+        {
+            var extractor = new NumericTokenExtractor();
+
+            var token = extractor.ExtractToken("-123.45", 0, new Context(ExpressiveOptions.None));
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual("-123.45", token.CurrentToken);
+        }
+
+        [TestMethod]
+        public void TestWithDecimalPlaceAndPositiveSign()
+        {
+            var extractor = new NumericTokenExtractor();
+
+            var token = extractor.ExtractToken("+123.45", 0, new Context(ExpressiveOptions.None));
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual("+123.45", token.CurrentToken);
+        }
+
+        [TestMethod]
         public void TestWithInteger()
         {
             var extractor = new NumericTokenExtractor();
@@ -26,6 +60,28 @@ namespace Expressive.Tests.Tokenisation
 
             Assert.IsNotNull(token);
             Assert.AreEqual("123", token.CurrentToken);
+        }
+
+        [TestMethod]
+        public void TestWithIntegerAndNegativeSign()
+        {
+            var extractor = new NumericTokenExtractor();
+
+            var token = extractor.ExtractToken("-123", 0, new Context(ExpressiveOptions.None));
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual("-123", token.CurrentToken);
+        }
+
+        [TestMethod]
+        public void TestWithIntegerAndPositiveSign()
+        {
+            var extractor = new NumericTokenExtractor();
+
+            var token = extractor.ExtractToken("+123", 0, new Context(ExpressiveOptions.None));
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual("+123", token.CurrentToken);
         }
 
         [TestMethod]
@@ -47,6 +103,50 @@ namespace Expressive.Tests.Tokenisation
 
             Assert.IsNotNull(token);
             Assert.AreEqual("123.45", token.CurrentToken);
+        }
+
+        [TestMethod]
+        public void TestWithScientificNotationAndNoOrderOfMagnitude()
+        {
+            var extractor = new NumericTokenExtractor();
+
+            var token = extractor.ExtractToken("1.23e", 0, new Context(ExpressiveOptions.None));
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual("1.23", token.CurrentToken);
+        }
+
+        [TestMethod]
+        public void TestWithScientificNotation()
+        {
+            var extractor = new NumericTokenExtractor();
+
+            var token = extractor.ExtractToken("1.23e2", 0, new Context(ExpressiveOptions.None));
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual("1.23e2", token.CurrentToken);
+        }
+
+        [TestMethod]
+        public void TestWithScientificNotationAndNegativeSign()
+        {
+            var extractor = new NumericTokenExtractor();
+
+            var token = extractor.ExtractToken("1.23e-2", 0, new Context(ExpressiveOptions.None));
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual("1.23e-2", token.CurrentToken);
+        }
+
+        [TestMethod]
+        public void TestWithScientificNotationAndPositiveSign()
+        {
+            var extractor = new NumericTokenExtractor();
+
+            var token = extractor.ExtractToken("1.23e+2", 0, new Context(ExpressiveOptions.None));
+
+            Assert.IsNotNull(token);
+            Assert.AreEqual("1.23e+2", token.CurrentToken);
         }
     }
 }
