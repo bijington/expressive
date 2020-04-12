30021a24a463dca41442b53fc2f028a4a608edc8
Shaun Lawrence <bijington@tinysoftltd.onmicrosoft.com>
2020-04-12 23:42:27 +0100

    Fixed broken unit test after a foolish global search and replace

--- Source/CSharp/Expressive/Expressive.Tests/ExpressionTests.cs
+++ Source/CSharp/Expressive/Expressive.Tests/ExpressionTests.cs
@@ -165,8 +165,8 @@ namespace Expressive.Tests
             Assert.AreEqual(true, new Expression("[date1] == '01/01/2016 00:00:00'").Evaluate(new Dictionary<string, object> { ["date1"] = new DateTime(2016, 01, 01) }));
 
             // Null safety
-            Assert.AreEqual(true, new Expression("[number1] is null").Evaluate(new Dictionary<string, object> { ["number1"] = null }));
-            Assert.AreEqual(false, new Expression("[number1] is null").Evaluate(new Dictionary<string, object> { ["number1"] = 2 }));
+            Assert.AreEqual(true, new Expression("[number1] == null").Evaluate(new Dictionary<string, object> { ["number1"] = null }));
+            Assert.AreEqual(false, new Expression("[number1] == null").Evaluate(new Dictionary<string, object> { ["number1"] = 2 }));
             Assert.AreEqual(false, new Expression("[number1] != null").Evaluate(new Dictionary<string, object> { ["number1"] = null }));
             Assert.AreEqual(true, new Expression("[number1] != null").Evaluate(new Dictionary<string, object> { ["number1"] = 2 }));
             Assert.AreEqual(true, new Expression("[number1] <> 2").Evaluate(new Dictionary<string, object> { ["number1"] = null }));
