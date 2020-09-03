3fcdc3f895268ca99793001bd112788938633daf
Shaun Lawrence <bijington@tinysoftltd.onmicrosoft.com>
2020-09-03 22:26:48 +0100

    Removed the old java code and shifted the source higher up the tree

---
+++ Source/coverage.ps1
@@ -0,0 +1,5 @@
+﻿dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+
+.\packages\ReportGenerator.3.1.2\tools\ReportGenerator.exe -reports:".\Expressive.Tests\coverage.netcoreapp3.0.opencover.xml" -targetdir:".\GeneratedReports\Output"
+
+Start-Process .\GeneratedReports\Output\index.htm
