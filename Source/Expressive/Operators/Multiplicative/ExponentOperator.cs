c7071a421dc3d4823c32d616f3248c69952bd1b0
Phil Graniero <pgraniero@agtgroup.com>
2021-01-04 14:43:55 -0500

    Add exponent operator

---
+++ Source/Expressive/Operators/Multiplicative/ExponentOperator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Expressive.Expressions;
+using Expressive.Expressions.Binary.Multiplicative;
+
+namespace Expressive.Operators.Multiplicative
+{
+    internal class ExponentOperator : OperatorBase
+    {
+        #region OperatorBase Members
+
+        public override IEnumerable<string> Tags => new[] { "^", "\u2038" };
+
+        public override IExpression BuildExpression(Token previousToken, IExpression[] expressions, Context context)
+        {
+            return new ExponentExpression(expressions[0], expressions[1], context);
+        }
+
+        public override OperatorPrecedence GetPrecedence(Token previousToken)
+        {
+            return OperatorPrecedence.Multiply;
+        }
+
+        #endregion
+    }
+}
