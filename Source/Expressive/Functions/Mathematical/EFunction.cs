b30c00021e9e1b83e4ddb0e0e12af6ce90042249
Phil Graniero <pgraniero@agtgroup.com>
2021-01-04 14:34:49 -0500

    Added E() and PI() functions

---
+++ Source/Expressive/Functions/Mathematical/EFunction.cs
@@ -0,0 +1,18 @@
+﻿using Expressive.Expressions;
+using Expressive.Helpers;
+using System;
+
+
+namespace Expressive.Functions.Mathematical
+{
+    internal class EFunction : FunctionBase
+    {
+        public override string Name { get { return "E"; } }
+
+        public override object Evaluate(IExpression[] parameters, Context context)
+        {
+            this.ValidateParameterCount(parameters, 0, 0);
+            return Math.E;
+        }
+    }
+}
