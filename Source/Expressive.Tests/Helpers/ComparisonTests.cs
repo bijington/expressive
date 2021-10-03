cad3cca2e6211a6e4d3bbaa65bdc613c941e113f
Shaun Lawrence <shaunrlawrence@gmail.com>
2021-10-03 15:00:34 +0100

    Exposed Comparison.CompareUsingMostPreciseType to the outside world

---
+++ Source/Expressive.Tests/Helpers/ComparisonTests.cs
@@ -0,0 +1,12 @@
+﻿using Expressive.Helpers;
+using NUnit.Framework;
+
+namespace Expressive.Tests.Helpers
+{
+    public static class ComparisonTests
+    {
+        [Test]
+        public static void CompareUsingMostPreciseTypeWithNullContextShouldThrowArgumentNullException() => 
+            Assert.That(() => Comparison.CompareUsingMostPreciseType(null, null, null), Throws.ArgumentNullException);
+    }
+}
